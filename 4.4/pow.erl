e6b9d5830782ab52dd5f7bba0e7baf0e2b5f5db3
Colin Detwiller <colinsr@me.com>
2016-03-11 07:08:13 -0500

    convert power_of custom to tail recursive

---
+++ 4.4/pow.erl
@@ -0,0 +1,26 @@
+%%%-------------------------------------------------------------------
+%%% @author Colin
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%% powers
+%%% @end
+%%% Created : 11. Mar 2016 6:47 AM
+%%%-------------------------------------------------------------------
+-module(pow).
+-author("Colin").
+
+%% API
+-export([raise/2]).
+
+-spec(raise(number(), number()) -> number()).
+
+%%  @doc Returns X to the Nth power in a tail recursive fashion :)
+
+raise(_X,N)  when N =:= 0 -> 1;
+raise(X, N)  when N =:= 1 -> X;
+raise(X, N)  when N > 0   -> raise(X, N, 1);
+raise(X, N)               -> 1.0 / raise(X, abs(N)).
+
+raise(_, N, Acc) when N =:= 0 -> Acc;
+raise(X, N, Acc) ->
+  raise(X, N-1, X * Acc).
